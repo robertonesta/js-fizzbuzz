2d18ab6f3236c736853cb517976dda40ca6cd6f4
RobertoNesta <robertonesta1993@gmail.com>
2023-03-10 16:56:09 +0100

    adding the if else

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,23 @@
-const ulElement = document.createElemente("ul");
-let fizz;
-let buzz;
-let fizzbuzz;
-const liElement = document.createElement("li")
-for (let i = 1; i < 101; i++) {
+const ulElement = document.createElement("ul");
+let fizz = "fizz";
+let buzz = "buzz";
+let fizzbuzz = "fizzbuzz";
 
-    if (i % 3 == 0) {
-        i = "fizz";
-    } else if (i % 5 == 0) {
-        i = "buzz";
-    } else if (i % 3 == 0 && i % 5 == 0){
+
+for (let i = 1; i < 101; i++) {
+    const liElement = document.createElement("li");
+    ulElement.append(liElement);
+    
+    if (i % 3 == 0 && i % 5 == 0) {
         i = "fizzbuzz";
+    } else if (i % 3 == 0) {
+        i = "fizz";
+    } else if (i % 5 == 0){
+        i = "buzz";
     }
 
-    liElement.append(i);
-    ulElement.append(liElement)
+    liElement.append(`${i}${fizz}${buzz}${fizzbuzz}`);
+   
 }
 
 console.log(ulElement);
