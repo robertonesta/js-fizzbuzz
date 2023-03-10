8d1ed307d4bbcbc7ebbbd96308cdb41b9355c5f7
RobertoNesta <robertonesta1993@gmail.com>
2023-03-10 18:26:43 +0100

    adding style

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,22 @@ let fizz = "fizz";
 let buzz = "buzz";
 let fizzbuzz = "fizzbuzz";
 
-
 for (let i = 1; i < 101; i++) {
     const liElement = document.createElement("li")
     ulElement.append(liElement)
 
     if (i % 3 == 0 && i % 5 == 0) {
         liElement.innerHTML = `${fizzbuzz}`
+        liElement.classList.add('fizzbuzz')
+
     } else if (i % 3 == 0) {
         liElement.innerHTML = `${fizz}`
+        liElement.classList.add('fizz')
+
     } else if (i % 5 == 0){
         liElement.innerHTML = `${buzz}`
+        liElement.classList.add('buzz')
+
     }
     else {
         liElement.innerHTML = `${i}`
