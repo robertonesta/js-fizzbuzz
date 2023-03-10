255b004d9d8c24c3172d0b07154e347fc631de7e
RobertoNesta <robertonesta1993@gmail.com>
2023-03-10 16:35:20 +0100

    setting fizz, buzz and fizzbuzz as let variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 const ulElement = document.createElemente("ul");
-
+let fizz;
+let buzz;
+let fizzbuzz;
+const liElement = document.createElement("li")
 for (let i = 1; i < 101; i++) {
 
-    const liElement = document.createElement("li")
+    if (i % 3 == 0) {
+        i = "fizz";
+    } else if (i % 5 == 0) {
+        i = "buzz";
+    } else if (i % 3 == 0 && i % 5 == 0){
+        i = "fizzbuzz";
+    }
 
     liElement.append(i);
     ulElement.append(liElement)
